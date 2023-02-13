5270f3e113cd900f21565f7e2dfb1be70766ee0d
huydayne168 <huynguyenviet1608@gmail.com>
2023-02-13 23:30:17 +0700

    check7

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
-// chuyển màu trang web:
+// khai báo biến:
 
 const body = document.querySelector("body");
 const timer = document.querySelector(".timer");
 const timeH = document.querySelector(".timer .time:nth-child(1)");
 const timeM = document.querySelector(".timer .time:nth-child(2)");
 const timeS = document.querySelector(".timer .time:nth-child(3)");
-const showBtn = document.querySelector(".show-btn");
+const arrowBtn = document.querySelector(".arrow-btn");
+const arrowBtnImg = document.querySelector(".arrow-btn img");
 
-const changColorWrapper = document.querySelector(".change-color--wrapper");
+const changeColor = document.querySelector(".change-color");
+const changeColorWrapper = document.querySelector(".change-color--wrapper");
 const colorBtn = document.querySelectorAll(".color-btn");
 
 const darkBtn = document.querySelector(".color-btn:nth-child(1)");
@@ -17,13 +19,34 @@ const summerBtn = document.querySelector(".color-btn:nth-child(4)");
 const fallBtn = document.querySelector(".color-btn:nth-child(5)");
 const winterBtn = document.querySelector(".color-btn:nth-child(6)");
 
+let changeColorWidth = changeColorWrapper.offsetWidth;
+
+var status = -1;
+arrowBtn.addEventListener("click", function () {
+    if (status == -1) {
+        changeColorWrapper.classList.add("trans");
+        arrowBtn.classList.add("trans");
+        arrowBtnImg.classList.add("hide-btn");
+
+        // changeColor.style = "overflow: visible";
+        status = 1;
+    } else if (status == 1) {
+        changeColorWrapper.classList.remove("trans");
+        arrowBtn.classList.remove("trans");
+        arrowBtnImg.classList.remove("hide-btn");
+        status = -1;
+    }
+    console.log(status);
+});
+
+// ==========chuyển màu trang web:
 darkBtn.addEventListener("click", function () {
     body.style = "background-color: var(--dark-body-color)";
     timeH.style = "background-color: var(--dark-time-color)";
     timeM.style = "background-color: var(--dark-time-color)";
     timeS.style = "background-color: var(--dark-time-color)";
-    changColorWrapper.style = "background-color: var(--dark-time-color)";
-    showBtn.style = "background-color: var(--dark-time-color)";
+    changeColorWrapper.style = "background-color: var(--dark-time-color)";
+    arrowBtn.style = "background-color: var(--dark-time-color)";
     timer.style = "color: #fff";
 });
 
@@ -32,8 +55,8 @@ whiteBtn.addEventListener("click", function () {
     timeH.style = "background-color: var(--white-time-color)";
     timeM.style = "background-color: var(--white-time-color)";
     timeS.style = "background-color: var(--white-time-color)";
-    changColorWrapper.style = "background-color: var(--white-time-color)";
-    showBtn.style = "background-color: var(--white-time-color)";
+    changeColorWrapper.style = "background-color: var(--white-time-color)";
+    arrowBtn.style = "background-color: var(--white-time-color)";
     timer.style = "color: #fff";
 });
 springBtn.addEventListener("click", function () {
@@ -41,8 +64,8 @@ springBtn.addEventListener("click", function () {
     timeH.style = "background-color: var(--spring-time-color)";
     timeM.style = "background-color: var(--spring-time-color)";
     timeS.style = "background-color: var(--spring-time-color)";
-    changColorWrapper.style = "background-color: var(--spring-time-color)";
-    showBtn.style = "background-color: var(--spring-time-color)";
+    changeColorWrapper.style = "background-color: var(--spring-time-color)";
+    arrowBtn.style = "background-color: var(--spring-time-color)";
     timer.style = "color: #AACB73";
 });
 summerBtn.addEventListener("click", function () {
@@ -50,8 +73,8 @@ summerBtn.addEventListener("click", function () {
     timeH.style = "background-color: var(--summer-time-color)";
     timeM.style = "background-color: var(--summer-time-color)";
     timeS.style = "background-color: var(--summer-time-color)";
-    changColorWrapper.style = "background-color: var(--summer-time-color)";
-    showBtn.style = "background-color: var(--summer-time-color)";
+    changeColorWrapper.style = "background-color: var(--summer-time-color)";
+    arrowBtn.style = "background-color: var(--summer-time-color)";
     timer.style = "color: #fff";
 });
 fallBtn.addEventListener("click", function () {
@@ -59,8 +82,8 @@ fallBtn.addEventListener("click", function () {
     timeH.style = "background-color: var(--fall-time-color)";
     timeM.style = "background-color: var(--fall-time-color)";
     timeS.style = "background-color: var(--fall-time-color)";
-    changColorWrapper.style = "background-color: var(--fall-time-color)";
-    showBtn.style = "background-color: var(--fall-time-color)";
+    changeColorWrapper.style = "background-color: var(--fall-time-color)";
+    arrowBtn.style = "background-color: var(--fall-time-color)";
     timer.style = "color: #fff";
 });
 winterBtn.addEventListener("click", function () {
@@ -68,9 +91,10 @@ winterBtn.addEventListener("click", function () {
     timeH.style = "background-color: var(--winter-time-color)";
     timeM.style = "background-color: var(--winter-time-color)";
     timeS.style = "background-color: var(--winter-time-color)";
-    changColorWrapper.style = "background-color: var(--winter-time-color)";
-    showBtn.style = "background-color: var(--winter-time-color)";
+    changeColorWrapper.style = "background-color: var(--winter-time-color)";
+    arrowBtn.style = "background-color: var(--winter-time-color)";
     timer.style = "color: #fff";
 });
+// ===============xong phần chuyển màu trang web
 
-console.log(timeH);
+// ===============ẩn hiện bảng chọn màu
